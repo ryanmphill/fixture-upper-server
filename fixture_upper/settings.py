8cd038b9904ea29d9002c8267ad8f8796c195667
Ryan Phillips <ryanmphill@gmail.com>
2023-10-16 14:28:37 -0500

    Updated path to deployed link in cors whitelist

--- fixture_upper/settings.py
+++ fixture_upper/settings.py
@@ -51,7 +51,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://localhost:4173',
-    'https://fixture-upper.netlify.app/'
+    'https://fixture-upper.netlify.app'
 )
 
 MIDDLEWARE = [
